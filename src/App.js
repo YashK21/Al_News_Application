50425198aa89a4113c3d6876c22048b75e358716
yashk21 <yashk8119@gmail.com>
2022-05-06 02:32:01 +0530

    Alan AI Added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
-
-function App() {
+import { useEffect } from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
+const alankey =
+  "dbabce39f80956a6b3fdad15ff31791e2e956eca572e1d8b807a3e2338fdd0dc/stage";
+const App = () => {
+  useEffect(() => {
+    // alanbtn
+    alanBtn({
+      key: alankey,
+      onCommand: ({ command }) => {
+        if (command === "testCommand") {
+          alert("Executed Successfully");
+        }
+      },
+    });
+  }, []);
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <>
+      <h1>Alan AI News Application</h1>
+    </>
   );
-}
+};
 
 export default App;
